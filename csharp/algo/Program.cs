a5941c34ae67d0b3774e0414ddd28201aa82bb9b
starr <starr@DESKTOP-P30J3OU>
2023-09-03 02:34:50 +0100

    first commit

--- csharp/algo/Program.cs
+++ csharp/algo/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace algo
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace algo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter a number");
+            string number = Console.ReadLine();
+            int intnumber = Convert.ToInt32(number);
+            int sum = 0;
+            for (int i = 1; i < intnumber; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+
+            }
+            Console.WriteLine(sum);
+
+
         }
+
+
     }
 }
